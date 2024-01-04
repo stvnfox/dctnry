2ec334a560002373b9fba459ea94a7dde16599a7
Steven <steven@stvnx.nl>
2024-01-04 19:18:26 +0100

    refactor: added play/pause icon instead of text + added some styling

--- src/components/ResultItem.tsx
+++ src/components/ResultItem.tsx
@@ -6,7 +6,7 @@ export const ResultItem = ({ result }: { result: DictionaryResult }) => {
 
 	return (
 		<section>
-			<div className="flex items-center gap-4">
+			<div className="flex items-center justify-between gap-4 mb-8">
 				<div>
 					<h2 className="text-2xl font-bold">{result.word}</h2>
 					<p className="text-lg">{result.phonetic}</p>
--- src/components/ResultItemAudioButton.tsx
+++ src/components/ResultItemAudioButton.tsx
@@ -37,9 +37,32 @@ export const ResultItemAudioButton = ({
 			<audio id={id} src={item.audio}>
 				<track kind="captions" />
 			</audio>
-			<button type="button" onClick={() => togglePlay(id)}>
-				{/* TODO: Add icon instead of text */}
-				{isPlaying ? "Pause" : "Play"}
+			<button
+				type="button"
+				className="group flex justify-center items-center bg-purple-200 hover:bg-purple-300 rounded-full transition-colors h-16 w-16"
+				onClick={() => togglePlay(id)}
+			>
+				{isPlaying ? (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						className="fill-purple-800 w-5 h-5"
+					>
+						<title>Pause</title>
+						<path d="M5.75 3a.75.75 0 0 0-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 0 0 .75-.75V3.75A.75.75 0 0 0 7.25 3h-1.5ZM12.75 3a.75.75 0 0 0-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 0 0 .75-.75V3.75a.75.75 0 0 0-.75-.75h-1.5Z" />
+					</svg>
+				) : (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						className="fill-purple-800 w-5 h-5"
+					>
+						<title>Play</title>
+						<path d="M6.3 2.84A1.5 1.5 0 0 0 4 4.11v11.78a1.5 1.5 0 0 0 2.3 1.27l9.344-5.891a1.5 1.5 0 0 0 0-2.538L6.3 2.841Z" />
+					</svg>
+				)}
 			</button>
 		</div>
 	);
