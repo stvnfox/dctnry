501ed600d7543a366d289981ff6a47a576add0e0
Steven <steven@stvnx.nl>
2024-01-06 20:03:08 +0100

    style: changed colors of icons

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -35,7 +35,7 @@ export const SearchBar = () => {
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 20 20"
 						fill="currentColor"
-						className="w-5 h-5"
+						className="fill-purple-500 w-5 h-5"
 					>
 						<title>Search</title>
 						<path
@@ -49,7 +49,7 @@ export const SearchBar = () => {
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 20 20"
 						fill="currentColor"
-						className="w-5 h-5"
+						className="fill-purple-500 w-5 h-5"
 					>
 						<title>Close</title>
 						<path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
